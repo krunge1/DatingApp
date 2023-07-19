164486f0b357156d404af4c431836a2fd6a926f5
Kyle Runge <krunge1@hotmail.com>
2023-07-19 07:59:38 -0500

    Set up server side fiule structure and base instal

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require('cors')
+const app = express();
+app.use(cors());
+
+require("./config/mongoose.config");
+
+app.use(express.json(), express.urlencoded({extended: true}));
+
+require("./routes/DatingApp.routes")(app);
+
+app.listen(8000, () => console.log("The server is all fired up on port 8000"));
