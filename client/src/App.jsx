2f158d431c7ba603fe9a28d017c2fbe9b1811050
Levi Josiah Mariano <lejosiahm@gmail.com>
2023-07-21 02:08:21 -0700

    build registration form component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import TestPage from "./components/TestPage";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <TestPage />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginForm/>} />
+                <Route path="/register" element={<RegisterForm/>} />
+                {/* <Route pathe="/dashboard" element={<HomePage/>}/> */}
+            </Routes>
+        
+        </BrowserRouter>
+            {/* <TestPage /> */}
         </>
     );
 }
