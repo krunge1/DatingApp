56745d90e86f2721438f858125d4bb5365a6f183
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-07-21 15:23:48 -0400

    adjust bg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,33 +10,8 @@ import Dashboard from "./components/Dashboard";
 function App() {
     const [count, setCount] = useState(0);
 
-    let background = () => {
-        let res = "";
-        let number = Math.ceil(Math.random() * 5);
-        switch (number) {
-            case 1:
-                res = "bg-pink-50";
-                break;
-            case 2:
-                res = "bg-fuchsia-50";
-                break;
-            case 3:
-                res = "bg-sky-50";
-                break;
-            case 4:
-                res = "bg-teal-50";
-                break;
-            case 5:
-                res = "bg-lime-50";
-                break;
-            default:
-                console.log("There must be some issue");
-        }
-        return res;
-    };
-
     return (
-        <div className={background()}>
+        <div className={"bg-gradient-to-r from-rose-100 to-teal-100"}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<LoginForm />} />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,7 @@ export default {
                 primary: "#fa9dab",
                 secondary: "#da9dcb",
                 dText: "#c467a2",
+                dBG: "linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248))",
             },
         },
     },
