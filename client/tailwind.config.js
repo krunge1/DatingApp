2ac1a9a3a4a8a06d717967fa72fb5dfee11b076d
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-07-21 15:29:49 -0400

    fix colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,9 +5,9 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: "#fa9dab",
-                secondary: "#da9dcb",
-                dText: "#c467a2",
+                primary: "#fabdcb",
+                secondary: "#ea9dcb",
+                dText: "#f487d2",
                 dBG: "linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248))",
             },
         },
