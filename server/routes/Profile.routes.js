eb50036aff12d010ba7d9f66354056dd6ea9b2df
Kyle Runge <krunge1@hotmail.com>
2023-07-22 10:20:06 -0500

    Update with file upload, still needs work

--- server/routes/Profile.routes.js
+++ server/routes/Profile.routes.js
@@ -5,5 +5,6 @@ module.exports = app => {
     app.get('/api/datingapp/profiles/:id', ProfileController.findProfileById);
     app.post('/api/datingapp/profiles/create', ProfileController.createProfile);
     app.post('/api/datingapp/profiles/update/:id', ProfileController.updateProfile);
+    app.post('/api/datingapp/profiles/uploadPhoto/:id', ProfileController.uploadPhoto);
     app.delete('/api/datingapp/profiles/delete/:id', ProfileController.deleteProfile);
 }
