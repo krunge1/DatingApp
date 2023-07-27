a78873aed2582eb0b6a2fc5672473e45fb95ad8f
Kyle Runge <krunge1@hotmail.com>
2023-07-27 15:49:18 -0500

    Profile Blind Date not work

--- server/controllers/Profile.controller.js
+++ server/controllers/Profile.controller.js
@@ -94,7 +94,7 @@ module.exports = {
             Profile.findOne({user: loggedInUserId})
             // console.log(userProfile)
             .then((userProfile) => {
-                res.json(userProfile, )
+                res.json(userProfile)
             })
             .catch((err) => {
                 res.status(500).json({message: 'Something went wrong', error: err})
